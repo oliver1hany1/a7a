c26f17e889677ca6e5a393508bdd49b3ebc84959
oliver1hany1 <45123922+oliver1hany1@users.noreply.github.com>
2018-11-17 21:25:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -566,23 +566,7 @@ m.sendMessage(args)
     
             
 	     
-client.on('message', message => {
-              if (!message.channel.guild) return;
-      if(message.content =='*member')
-      var IzRo = new Discord.RichEmbed()
-      .setThumbnail(message.author.avatarURL)
-      .setFooter(message.author.username, message.author.avatarURL) 
-      .setTitle('🌷| Members info')
-      .addBlankField(true)
-      .addField('📗| Online',
-      `${message.guild.members.filter(m=>m.presence.status == 'online').size}`)
-      .addField('📕| DND',`${message.guild.members.filter(m=>m.presence.status == 'dnd').size}`)
-      .addField('📙| Idle',`${message.guild.members.filter(m=>m.presence.status == 'idle').size}`)
-      .addField('📓| Offline',`${message.guild.members.filter(m=>m.presence.status == 'offline').size}`)
-      .addField('➡| Server Members',`${message.guild.memberCount}`);
-      message.channel.send(IzRo);
-	
-    });
+
 
 
 client.on('message', message => {
